b897139a088f717ff566bc949efeb0b463ee88c2
jeffmon <jeffmon@gmail.com>
2017-05-18 20:21:35 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = function(){
+  var storeCode = {};
+  var setValue = function(key, value){
+    storeCode[key] = value;
+    return storeCode;
+  }
+  var getValue = function(key){
+    if(storeCode.hasOwnProperty(key)){
+      return storeCode[key];
+    }else{
+      return null;
+    }
+  }
+  return {
+    setValue:setValue,
+    getValue:getValue
+  }
+}
